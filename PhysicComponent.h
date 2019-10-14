dc0f26f75ada7a0b4ed68819ae4a9cb469060f42
Corentin Guillerme <corentin.g@net-c.com>
2019-10-14 23:40:55 +0200

    add PhysicComponent

---
+++ PhysicComponent.h
@@ -0,0 +1,17 @@
+#ifndef GRAPHIC_COMP_H
+#define GRAPHIC_COMP_H
+
+#include <forward_list>
+
+class Graphic
+{
+    public:
+        float m_x;
+        float m_y;
+        bool m_gravity;
+    private:
+        
+};
+
+
+#endif
