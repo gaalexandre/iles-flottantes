994bf1b06232d1ed93f95e417eeed152c485524c
Lucas <lucasbourel98@gmail.com>
2019-10-30 21:49:59 +0100

    *

---
+++ src/PlayerPhysic.h
@@ -0,0 +1,44 @@
+//
+//  PlayerPhysic.hpp
+//  
+//
+//  Created by Lucas Bourel on 24/10/2019.
+//
+
+#ifndef PlayerPhysic_hpp
+#define PlayerPhysic_hpp
+
+#include <SFML/Graphics.hpp>
+#include "PhysicComponent.h"
+#include <SFML/System.hpp>
+
+#include <stdio.h>
+
+class PlayerPhysic : public
+{
+public:
+    
+    PlayerPhysic()
+
+    virtual void update(sf::Time t);
+    virtual void collide( PhysicComponent &other);
+    
+
+protected:
+
+    virtual bool intersect(sf::Vector2f point);
+    virtual bool intersect(sf::FloatRect rect);
+    
+private:
+    float sf::floatRect m_hitBox{0, 0, 0, 0};
+    float sf::floatRect m_hitBox_tmp{0, 0, 0, 0};
+    float m_accelerationX{0};
+    float m_accelerationY{0};
+    
+    
+}
+
+
+
+
+#endif /* PlayerPhysic_hpp */
