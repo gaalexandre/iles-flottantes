90c9db37870a84c058d0d52770cbd2a3f57c060b
alexandre <aran.alexandre@gmail.com>
2019-11-27 09:39:49 +0100

    temp

--- src/Jeu.cpp
+++ src/Jeu.cpp
@@ -2,31 +2,27 @@
 #include "Map.h"
 #include "Perso.h"
 
+#include <string>
 #include <iostream>
+#include <fstream>
 
 int Jeu::gameLoop()
 {
   m_window.setView(m_view);
-  Map map("mapExemple");
-  Perso perso("persoExample");
-  perso.loadGraphicComponent(m_graphicModule);
-    perso.loadPhysicComponent(m_physicModule);
-    perso.loadSystemComponent(m_systemModule);
-    
-  map.loadGraphicComponent(m_graphicModule);
-    map.loadPhysicComponent(m_physicModule);
-    
-    sf::Clock timer;
-    
+
+  sf::Clock timer;
+  loadNextLevel();
   while (m_window.isOpen())
   {
     eventLoop();
     m_systemModule.manageEvent(m_event);
-      
+
     m_physicModule.update( timer.restart() );
-    
+
     m_window.clear(sf::Color::Black);
+
     m_graphicModule.draw(m_window);
+
     m_window.display();
   }
   return 0;
@@ -35,26 +31,42 @@ int Jeu::gameLoop()
 void Jeu::eventLoop()
 {
   while (m_window.pollEvent(m_event))
+  {
+    switch (m_event.type)
     {
-        
-      switch (m_event.type)
-	{
-	case sf::Event::Closed:
-	  m_window.close();
-	  break;
-
-	case sf::Event::KeyPressed:
-           
-	  break;
-	case sf::Event::Resized:
-    m_view.setSize(m_event.size.width, m_event.size.height);
-    m_view.zoom(2);
-    m_window.setView(m_view);
-	  
-	  break;
-	default:
-            
-	  break;
-	}
+    case sf::Event::Closed:
+      m_window.close();
+      break;
+      
+    case sf::Event::KeyPressed:
+      
+      break;
+    case sf::Event::Resized:
+      m_view.setSize(m_event.size.width, m_event.size.height);
+      m_view.zoom(2);
+      m_window.setView(m_view);
+      
+      break;
+    default:
+      
+      break;
     }
+  }
+}
+
+void Jeu::loadNextLevel()
+{
+  m_levelNumber++;
+  std::ifstream levelFile("level" + std::to_string(m_levelNumber));
+  std::string mapFileName, playerFilename;
+  levelFile >> mapFileName >> playerFilename;
+
+  Map *map=new Map(mapFileName);
+  Perso *perso=new Perso(playerFilename);
+
+  perso->loadGraphicComponent(m_graphicModule);
+  perso->loadPhysicComponent(m_physicModule);
+  perso->loadSystemComponent(m_systemModule);
+  map->loadGraphicComponent(m_graphicModule);
+  map->loadPhysicComponent(m_physicModule);
 }
--- src/Jeu.h
+++ src/Jeu.h
@@ -3,7 +3,6 @@
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-
 #include "GameComponent.h"
 #include "PhysicModule.h"
 #include "SystemModule.h"
@@ -13,17 +12,16 @@ class Jeu
 {
  public:
   int gameLoop();
-  
  private:
   void eventLoop();
-  
+  void loadNextLevel();
   sf::RenderWindow m_window{sf::VideoMode(800, 800), "Ile flotante!"};
-  sf::Event m_event;
+  sf::Event m_event; 
   sf::View m_view{sf::FloatRect(0.f, 0.f, 8*256.f, 8*256.f)};
+  int m_levelNumber{0};
   GraphicModule m_graphicModule;
   PhysicModule m_physicModule;
   SystemModule m_systemModule;
 };
-  
 
 #endif
